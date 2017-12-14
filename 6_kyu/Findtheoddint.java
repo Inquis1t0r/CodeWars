1e818a5659f1574a7fe82134fdb93c34d0a86024
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2017-12-14 22:34:44 +0100

    Update Findtheoddint.java

--- 6_kyu/Findtheoddint.java
+++ 6_kyu/Findtheoddint.java
@@ -1,16 +1,1 @@
-public class FindOdd {
-	public static int findIt(int[] A) {
-  int odd = 0, i = 0;
- 
- //TODO: check and refactor
- 
-  while(i < A.length){
-     if(A[i] % 2 == 1){
-        odd = A[i];
-        }
-     i++;
-     }
-   return odd;
-  }
-  
-}
+ //TODO
