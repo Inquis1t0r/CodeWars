aa98a1414108ec22038208107a6f9581eaab42e9
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-03 20:19:52 +0100

    Some cleanup

--- 7_kyu/NumberofDecimalDigits.java
+++ 7_kyu/NumberofDecimalDigits.java
@@ -1,6 +1,5 @@
 public class DecTools {
   public static int Digits(long n) {
-    int temp = 0;
 //TODO: integer underflow/overflow  check/prevention !
     String s = Integer.toString(Math.toIntExact(n)); 
 
