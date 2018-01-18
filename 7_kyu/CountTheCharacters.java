9cb394f2e662099ca699b7869014d06228c89871
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-18 19:47:17 +0100

    Refactor

--- 7_kyu/CountTheCharacters.java
+++ 7_kyu/CountTheCharacters.java
@@ -1,13 +1,10 @@
 public class CC {
   public static int charCount(String str, char c) {
+    str = str.toLowerCase();
+    c = Character.toLowerCase(c);
     int count = 0;
-  
-    for (int i = 0; i < str.length(); i++) {
-      if (Character.toLowerCase(str.charAt(i)) == c) {
-          count++;
-    }
-}
-  return count;
+    for(int i = 0; i < str.length(); i++)
+      if(str.charAt(i) == c)count++;
+    return count;
   }
-
 }
