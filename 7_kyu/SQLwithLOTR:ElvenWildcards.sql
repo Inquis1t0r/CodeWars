d18e094374d5aaac9a451a2e46f03981839bb2a3
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-06-25 14:27:20 +0200

    Update SQLwithLOTR:ElvenWildcards.sql

--- 7_kyu/SQLwithLOTR:ElvenWildcards.sql
+++ 7_kyu/SQLwithLOTR:ElvenWildcards.sql
@@ -1,2 +1,2 @@
 --but on the land of Lórien no shadow lay--
-SELECT INITCAP(Elves.firstname) || ' ' || INITCAP(Elves.lastname) AS shortlist FROM Elves; 
+SELECT INITCAP(Elves.firstname) || ' ' || INITCAP(Elves.lastname) AS shortlist FROM Elves WHERE Elves.firstname LIKE '%tegil%' OR Elves.lastname LIKE '%astar%'; 
