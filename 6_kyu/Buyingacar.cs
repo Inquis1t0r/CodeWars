13649e7eb9b33d8666f7d238d06f0adaef2bbb72
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2019-05-04 20:23:17 +0200

    Update Buyingacar.cs

--- 6_kyu/Buyingacar.cs
+++ 6_kyu/Buyingacar.cs
@@ -1,8 +1,29 @@
+using System;
+
 public class BuyCar {
 
 	public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingPerMonth, double percentLossByMonth) {
-		// your code
-		//TODO!
-		return null;
+
+    int months = 0;
+    int savingsTotal = 0;
+    
+    while(savingsTotal + startPriceOld < startPriceNew)
+    {
+      months ++;
+      if (months % 2 == 0) {
+        percentLossByMonth += 0.5; 
+      }
+    }
+    
+    savingsTotal += savingPerMonth;
+    
+    startPriceOld -= startPriceOld * (int)(percentLossByMonth / 100);
+    startPriceNew -= startPriceNew * (int)(percentLossByMonth / 100);
+    
+    int result = Convert.ToInt16(Math.Round((double)savingsTotal + startPriceOld - startPriceNew));
+    
+    int[] array = new int[2] {months, result};
+    
+		return array;
 	}
 }
