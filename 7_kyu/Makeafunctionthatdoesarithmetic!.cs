6261571f20bc9a95f08372133e524824d7bbe777
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-07-11 11:00:53 +0200

    Update Makeafunctionthatdoesarithmetic!.cs

--- 7_kyu/Makeafunctionthatdoesarithmetic!.cs
+++ 7_kyu/Makeafunctionthatdoesarithmetic!.cs
@@ -4,6 +4,28 @@ public class Kata
 {
   public static double Arithmetic(double a, double b, string op) 
   {
-    throw new NotImplementedException();
+    //throw new NotImplementedException();
+    double result = 0;
+    
+    switch (op) { 
+              
+        case "add": 
+            result = a + b; 
+            break; 
+  
+        case "subtract": 
+            result = a - b;
+            break; 
+            
+        case "multiply": 
+            result = a * b;
+            break; 
+            
+        case "divide": 
+            result = a/b;
+            break; 
+        } 
+    
+    return result;
   }
 }
