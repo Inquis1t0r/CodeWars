f7662d1d2dc8d8ff40a851cd5bdf9b7cae275aa4
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-07-15 09:45:03 +0200

    Update Vasya-Clerk.cs

--- 6_kyu/Vasya-Clerk.cs
+++ 6_kyu/Vasya-Clerk.cs
@@ -5,17 +5,16 @@ public class Line
         public static string Tickets(int[] peopleInLine)
         { 
             int sumAtClerk = 0;
-            Boolean check = false;
+            int check = 0;
             
             for (int i = 0; i < peopleInLine.Length; i++)
               {
                 sumAtClerk += 25;
                 
                 if(peopleInLine[i] - 25 <= sumAtClerk)
-                check = true;
-                else
-                check = false;
+                check += 1;
+
               }
-        return  check != false ? "YES" : "NO";
+        return  check == peopleInLine.Length ? "YES" : "NO";
         }
-    }
+}
