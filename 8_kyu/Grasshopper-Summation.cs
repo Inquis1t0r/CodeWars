97b700f261d1e36f551c314841a8f639d3b6c87e
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-08-18 13:39:29 +0200

    Update Grasshopper-Summation.cs

--- 8_kyu/Grasshopper-Summation.cs
+++ 8_kyu/Grasshopper-Summation.cs
@@ -4,6 +4,10 @@ public static class Kata
 {
     public static int summation(int num)
     {
-      //Code here
+      int result = 0;
+      for(int i = num; i > 0; i--)
+      result += i;
+      
+      return result;
     }
 }
