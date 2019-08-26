30414ab9752fdea0a6dae2e6cb64a792425469b2
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-08-26 10:25:12 +0200

    Update BreakcamelCase.cs

--- 6_kyu/BreakcamelCase.cs
+++ 6_kyu/BreakcamelCase.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Text;
+
 public class Kata
 {
   public static string BreakCamelCase(string str)
   {
-    // complete the function
+    StringBuilder sb = new StringBuilder("");
+    
+    foreach (char c in str)
+    {
+       if(Char.IsUpper(c))
+       sb.Append(" "+ c.ToString());
+       else 
+       sb.Append(c.ToString());
+    }
+    return sb.ToString();
   }
 }
