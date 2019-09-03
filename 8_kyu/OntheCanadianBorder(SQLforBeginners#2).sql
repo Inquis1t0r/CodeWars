e2208d0eca3b29516a7b486c7db02283a467924a
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-09-03 15:26:16 +0200

    refactor

--- 8_kyu/OntheCanadianBorder(SQLforBeginners#2).sql
+++ 8_kyu/OntheCanadianBorder(SQLforBeginners#2).sql
@@ -1,4 +1,4 @@
 --Your Code Here
-SELECT  * 
-FROM travelers   
-WHERE not  country = 'USA' and not country = 'Canada' and not country = 'Mexico'; 
+SELECT *
+FROM travelers
+WHERE country NOT IN ('Canada', 'Mexico', 'USA')
