b93377dfca52262f10db140f604a4921a3140467
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-11-13 10:22:38 +0100

    Create CalculateBMI.swift

---
+++ 8_kyu/CalculateBMI.swift
@@ -0,0 +1,20 @@
+func bmi(_ weight: Int, _ height: Double) -> String {
+    # your code
+    
+    var result: String
+    var BMI = Double(weight) / height ^ 2
+
+    if BMI <= 18.5 {
+        result = "Underweight"
+    } 
+    else if BMI <= 25.0 {
+        result = "Normal"
+    } 
+    else if BMI <= 30.0  {
+        result = "Overweight"
+    }
+    else {
+        result = "Obese"
+    }
+    return result
+}
