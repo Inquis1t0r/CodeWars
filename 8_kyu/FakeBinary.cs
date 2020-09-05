546c80c39acfef2ec957b721a4158e875efee2f4
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-09-05 12:23:38 +0200

    Create FakeBinary.cs

---
+++ 8_kyu/FakeBinary.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Kata
+{
+  public static string FakeBin(string x)
+  {
+    string result = String.Empty;
+    foreach(char c in x)
+    {
+      if
+      result += c;
+    }
+    
+    return "";
+  }
+}
