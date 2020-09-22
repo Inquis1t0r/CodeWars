af016724ea69c0b42ef79ca11fb03ba5691e1bd8
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-09-22 14:15:46 +0200

    Update Summinganumber'sdigits.cs

--- 7_kyu/Summinganumber'sdigits.cs
+++ 7_kyu/Summinganumber'sdigits.cs
@@ -3,9 +3,5 @@ using System.Linq;
 
 public class Kata
 {
-  public static int SumDigits(int number)
-  {
-    int[] digits = number.ToString().Replace("-", "").Select(c => Convert.ToInt32(c.ToString())).ToArray();
-    return digits.Sum();
-  }
+  public static int SumDigits(int number) => number.ToString().Replace("-", "").Select(c => Convert.ToInt32(c.ToString())).ToArray().Sum();
 }
