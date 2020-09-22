88b98ea0a7bea1847a21f1c53d1e557841dfa7aa
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-09-22 14:15:05 +0200

    Update Summinganumber'sdigits.cs

--- 7_kyu/Summinganumber'sdigits.cs
+++ 7_kyu/Summinganumber'sdigits.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Linq;
+
 public class Kata
 {
   public static int SumDigits(int number)
   {
-    
+    int[] digits = number.ToString().Replace("-", "").Select(c => Convert.ToInt32(c.ToString())).ToArray();
+    return digits.Sum();
   }
 }
