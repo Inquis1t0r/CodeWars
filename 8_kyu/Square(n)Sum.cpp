edbf494d8cc9bf5b6a2f34b722a683193294eb7e
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-10-05 14:08:43 +0200

    Update Square(n)Sum.cpp

--- 8_kyu/Square(n)Sum.cpp
+++ 8_kyu/Square(n)Sum.cpp
@@ -1,11 +1,12 @@
 #include <vector>
+#include <cmath>
 
 int square_sum(const std::vector<int>& numbers)
 {
   int sum = 0;
   for(int number : numbers)
     {
-      sum += number ^ 2;
+      sum += pow(number, 2);
     }
   
     return sum;
