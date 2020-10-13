5df2db1a7f1d965a72bce5fb0aca4f3150928bac
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-10-13 09:17:50 +0200

    Update Function1-helloworld.cs

--- 8_kyu/Function1-helloworld.cs
+++ 8_kyu/Function1-helloworld.cs
@@ -1,11 +1,11 @@
-using System.Text;
+using System;
 
 public static class Kata
 {
 
     public static string greet() 
     {
-      string mystery = "68 65 6c 6c 6f 20 77 6f 72 6c 64 21";
-      return Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(mystery));
+      string mystery = "01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 00100001";
+      return Convert.ToBase64String(Encoding.ASCII.GetBytes(mystery));
     }
 }
