587587c60f6c8c6096c51bc1fbec12044ac31ef8
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-10-13 09:14:24 +0200

    Update Function1-helloworld.cs

--- 8_kyu/Function1-helloworld.cs
+++ 8_kyu/Function1-helloworld.cs
@@ -1,4 +1,11 @@
+using System.Text;
+
 public static class Kata
 {
-  // Write a public static method "greet" that returns "hello world!"
+
+    public static string greet() 
+    {
+      string mystery = "68 65 6c 6c 6f 20 77 6f 72 6c 64 21";
+      return Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(mystery));
+    }
 }
