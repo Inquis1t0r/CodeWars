d07b16a0822c52789afee14ff07bb1c42f26deec
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-10-29 10:08:14 +0100

    Create Transportationonvacation.swift

---
+++ 8_kyu/Transportationonvacation.swift
@@ -0,0 +1,12 @@
+func RentalCarCost(_ days: Int) -> Int {
+  var discount = 0
+  
+  if(days >= 7 ){
+    discount += 50
+  }
+ else if(days >= 3){
+    discount += 20
+  }
+  
+  return (days * 40) - discount
+}
