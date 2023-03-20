9231aaa98fb682256341a525f16a838d028b29ef
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-03-20 14:35:49 +0100

    Update FindthemissingterminanArithmeticProgression.swift

--- 6_kyu/FindthemissingterminanArithmeticProgression.swift
+++ 6_kyu/FindthemissingterminanArithmeticProgression.swift
@@ -1,2 +1,12 @@
-func find_missing(l:[Int]) -> Int {
+func findMissingTerm(_ l: [Int]) -> Int? {
+    let n = l.count
+    let commonDiff = (l[n-1] - l[0]) / n // calculate common difference
+
+    for i in 0..<n-1 {
+        let expectedNext = l[i] + commonDiff // calculate what the next number should be
+        if l[i+1] != expectedNext {
+            return expectedNext // return the missing number
+        }
+    }
+    return nil // return nil if no missing number is found
 }
