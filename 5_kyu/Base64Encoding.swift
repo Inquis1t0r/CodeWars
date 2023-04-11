ec5d7075fa17e8bb92bf05e342ffb3e317210526
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-04-11 20:30:15 +0200

    Update Base64Encoding.swift

--- 5_kyu/Base64Encoding.swift
+++ 5_kyu/Base64Encoding.swift
@@ -1,11 +1,15 @@
 extension String {
 	public var toBase64: String {
-		// Do something!
-    return self
+        guard let data = self.data(using: .ascii) else {
+            return ""
+        }
+        return data.base64EncodedString(options: [])
 	}
 	
 	public var fromBase64: String {
-    // Do something!
-		return self
+        guard let data = Data(base64Encoded: self) else {
+            return ""
+        }
+        return String(data: data, encoding: .ascii) ?? ""
 	}
 }
