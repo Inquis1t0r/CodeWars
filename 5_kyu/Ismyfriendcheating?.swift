74bc533db9b84b4ed7d6ec831e2650ea8bf829c7
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-04-12 15:13:26 +0200

    Update Ismyfriendcheating?.swift

--- 5_kyu/Ismyfriendcheating?.swift
+++ 5_kyu/Ismyfriendcheating?.swift
@@ -1,3 +1,11 @@
 func removNb(_ n: Int) -> [(Int,Int)] {
-    //your code
+    let sum = (1...n).reduce(0, +)
+    var result = [(Int, Int)]()
+    for a in 1...n {
+        let b = (sum - a) / (a + 1)
+        if a * b + a + b == sum && b <= n {
+            result.append((a, b))
+        }
+    }
+    return result
 }
