0997dd75316f5df9fe7a511eb2b6024bfc771f6a
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-04-13 16:57:35 +0200

    Update CalculateStringRotation.cs

--- 6_kyu/CalculateStringRotation.cs
+++ 6_kyu/CalculateStringRotation.cs
@@ -1,7 +1,23 @@
 public class CalculateStringRotation
 {
-  public static int ShiftedDiff(string first, string second)
+  public static int? ShiftedDiff(string first, string second)
   {
-    return 0;
+      if (first.Length != second.Length)
+      {
+          return null;
+      }
+
+      string combined = first + first;
+      for (int i = 0; i < first.Length; i++)
+      {
+          string substring = combined.Substring(i, first.Length);
+          if (substring == second)
+          {
+              return i;
+          }
+      }
+
+      return null;
   }
+
 }
