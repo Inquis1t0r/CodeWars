bd0dc3035a0e9858eaf77a747612689e0c055b03
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-04-16 19:22:21 +0200

    Update Isanumberprime?.js

--- 6_kyu/Isanumberprime?.js
+++ 6_kyu/Isanumberprime?.js
@@ -1,4 +1,16 @@
-
 function isPrime(num) {
-  //TODO
+  // negative numbers, 0, and 1 are not prime
+  if (num <= 1) {
+    return false;
+  }
+  
+  // check if num is divisible by any number from 2 to the square root of num
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  
+  // if we haven't returned false yet, num is prime
+  return true;
 }
