d6cd259fb78ba0c657da6b27f43ac2c3eff22cd1
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-06-17 06:57:51 +0200

    Update Howgoodareyoureally?.js

--- 8_kyu/Howgoodareyoureally?.js
+++ 8_kyu/Howgoodareyoureally?.js
@@ -1,3 +1,5 @@
 function betterThanAverage(classPoints, yourPoints) {
   // Your code here
+  const average = classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+  return yourPoints > average;
 }
