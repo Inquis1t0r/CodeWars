e7f072565f7fa63845444b35a638ade76d4b4bcb
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-08-29 03:01:31 +0200

    Update OppositesAttract.cs

--- 8_kyu/OppositesAttract.cs
+++ 8_kyu/OppositesAttract.cs
@@ -2,6 +2,7 @@ using System;
 
 public class LoveDetector
 {
+    //public static bool lovefunc(int f1, int f2)=>(f1+f2)%2!=0;
     public static bool lovefunc(int flower1, int flower2)
     { 
         //Moment of truth...
