1c1da8ac20037ded3f4775c9b846d53933d42ebe
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-03-21 14:41:52 +0100

    Update ExesandOhs.js

--- JavaScript/7_kyu/ExesandOhs.js
+++ JavaScript/7_kyu/ExesandOhs.js
@@ -1,3 +1,10 @@
 function XO(str) {
     //code here
+    let countX = 0;
+    let countO = 0;
+      for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            countX++;
+        }
+    }
 }
