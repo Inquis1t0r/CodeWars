945d50408892c4088da29be13973cd6e88377bff
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-03-22 12:15:11 +0100

    Update ExesandOhs.js

--- JavaScript/7_kyu/ExesandOhs.js
+++ JavaScript/7_kyu/ExesandOhs.js
@@ -3,8 +3,12 @@ function XO(str) {
     let countX = 0;
     let countO = 0;
       for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
+        if (str.charAt(i).toLowerCase() === "x") {
             countX++;
         }
+        if (str.charAt(i).toLowerCase() === "o") {
+            countO++;
+        }
     }
+  return countX == countO;
 }
