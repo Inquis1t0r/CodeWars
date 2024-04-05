6d6331a9f3e0124c0c4d33533f0a862533ea88c6
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-04-05 09:22:28 +0200

    Update Invertvalues.js

--- JavaScript/8_kyu/Invertvalues.js
+++ JavaScript/8_kyu/Invertvalues.js
@@ -1,9 +1,7 @@
 function invert(array) {
   let result = [];
   
-  array.forEach(function(ele) {
-      result.push(ele * -1);
-    });
-  
+  array.forEach(ele => result.push(ele * -1));
+
    return result;
 }
