1c0fbf3fede0fdc213c80b703f1614f33e265793
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-10 07:59:13 +0200

    Update Money,Money,Money.js

--- JavaScript/7_kyu/Money,Money,Money.js
+++ JavaScript/7_kyu/Money,Money,Money.js
@@ -1,3 +1,17 @@
 function calculateYears(principal, interest, tax, desired) {
     // your code
+      if (principal === desired) {
+        return 0;
+    }
+
+    let years = 0;
+
+    while (principal < desired) {
+        const interestEarned = principal * interest;
+        const taxPaid = interestEarned * tax;
+        principal += interestEarned - taxPaid;
+        years++;
+    }
+
+    return years;
 }
