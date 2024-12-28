11589e973be75c9c01a53e6914c89eb12c45dc08
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-12-28 06:47:49 +0100

    Update DoubleChar.java

--- Java/8_kyu/DoubleChar.java
+++ Java/8_kyu/DoubleChar.java
@@ -1,5 +1,12 @@
-public class Solution{
-  public static String doubleChar(String s){
-    //enter your code here 
-  }
+public class Solution {
+    public static String doubleChar(String s) {
+        String result = new String();
+        //enter your code here 
+        for (char tempChar: s.toCharArray()) {
+            System.out.println(tempChar);
+            String character = String.valueOf(tempChar);
+            result += character + character;
+        }
+        return result;
+    }
 }
