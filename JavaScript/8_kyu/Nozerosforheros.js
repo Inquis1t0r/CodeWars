c1b3801a39b15c260d7161aca7e9c5084411dd59
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-01-01 22:14:03 +0100

    Update Nozerosforheros.js

--- JavaScript/8_kyu/Nozerosforheros.js
+++ JavaScript/8_kyu/Nozerosforheros.js
@@ -1,3 +1,8 @@
 function noBoringZeros(n) {
   // your code
+  let stringFromNum = n.toString();
+  while(stringFromNum.charAt(stringFromNum.length-1) == 0){
+    stringFromNum = stringFromNum.slice(0,-1);
+  }
+  return Number(stringFromNum);
 }
