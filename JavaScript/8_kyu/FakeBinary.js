28fedc437e2539ca69d76cd2dd1dd8177923989e
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-01-03 20:20:38 +0100

    Update FakeBinary.js

--- JavaScript/8_kyu/FakeBinary.js
+++ JavaScript/8_kyu/FakeBinary.js
@@ -1,3 +1,11 @@
-function fakeBin(x){
-  
+function fakeBin(x) {
+    let result = "";
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] < 5) {
+            result += 0;
+        } else {
+            result += 1;
+        }
+    }
+    return result;
 }
