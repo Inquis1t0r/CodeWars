cda8ca5f9043d601b31df96d1775853bfa44ef4a
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-01-12 07:24:14 +0100

    Update FindtheRemainder.js

--- JavaScript/8_kyu/FindtheRemainder.js
+++ JavaScript/8_kyu/FindtheRemainder.js
@@ -1,4 +1,11 @@
 function remainder(n, m){
   // Divide the larger argument by the smaller argument and return the remainder
+    if (n === 0 || m === 0) {
+        return NaN;
+    }
 
+    const larger = Math.max(n, m);
+    const smaller = Math.min(n, m);
+
+    return larger % smaller;
 }
