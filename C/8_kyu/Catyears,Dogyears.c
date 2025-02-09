d6defdc75dfd651af6ebd6ca45343fdc28264c52
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-02-09 08:27:06 +0100

    Create Catyears,Dogyears.c

---
+++ C/8_kyu/Catyears,Dogyears.c
@@ -0,0 +1,9 @@
+typedef struct Human_Cat_Dog_Years {
+    unsigned human_years, cat_years, dog_years;
+} years;
+
+years human_years_cat_years_dog_years(unsigned human) {
+
+    //  <----  hajime!
+
+}
