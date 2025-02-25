8950a82d0d5117243f2670785a39542f12574a4e
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-02-25 15:57:36 +0100

    Update Factorial.swift

--- Swift/7_kyu/Factorial.swift
+++ Swift/7_kyu/Factorial.swift
@@ -1,5 +1,9 @@
 func factorial(_ n: Int) -> UInt64 {
-  return 0
+  var result = 0
+  for i in 1...n {
+      result = result * i
+  }
+  return UInt64(result)
 }
 
 // If "random tests" exits with code 132, it likely means that
