d033dbce99afd62b80095b3f540fdb03c335c174
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-03-14 19:31:24 +0100

    Update V A P O R C O D E.swift

--- Swift/7_kyu/V A P O R C O D E.swift
+++ Swift/7_kyu/V A P O R C O D E.swift
@@ -1,3 +1,6 @@
 func vaporcode(_ str: String) -> String {
-    ""
+    let noWhitespace = str.replacingOccurrences(of: " ", with: "") 
+    let uppercased = noWhitespace.uppercased() 
+    let spacedOut = uppercased.map { "\($0)  " }.joined()
+    return spacedOut.trimmingCharacters(in: .whitespaces) 
 }
