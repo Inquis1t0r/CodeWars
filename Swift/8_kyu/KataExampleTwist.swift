b304a67c1a0cd24a540525ea27cfa6711de8a78b
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-03-22 18:49:29 +0100

    Update KataExampleTwist.swift

--- Swift/8_kyu/KataExampleTwist.swift
+++ Swift/8_kyu/KataExampleTwist.swift
@@ -1,3 +1,6 @@
 var websites: [String] = []
 // Top-level iterative code is not allowed so solving this kata
 // probably requires editing the initializer
+for _ in 1...1000 {
+  websites.append("codewars")
+}
