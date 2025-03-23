38bda9bcaf1ea55ef8b676f6002a5d0855677ac9
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-03-23 08:19:39 +0100

    Update KataExampleTwist.js

--- JavaScript/8_kyu/KataExampleTwist.js
+++ JavaScript/8_kyu/KataExampleTwist.js
@@ -3,3 +3,8 @@ let websites = []
 for (let i = 0; i < 1000; i++) {
   websites.push("codewars");
 }
+
+
+//var websites = new Array(1000).fill("codewars");
+//var websites = Array(1000).fill("codewars");
+//var websites = Array.from({ length: 1000 }, () => 'codewars')
