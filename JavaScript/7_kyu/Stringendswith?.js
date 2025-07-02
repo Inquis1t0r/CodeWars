21b9ba8083606a0bd24c12ad285c973b6d5251a8
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-07-02 17:34:14 +0200

    Update Stringendswith?.js

--- JavaScript/7_kyu/Stringendswith?.js
+++ JavaScript/7_kyu/Stringendswith?.js
@@ -1,3 +1,7 @@
 function solution(str, ending){
   // TODO: complete
+  const endingLength = ending.length;
+
+  const strEnding = str.substring(endingLength-1, str.length);
+  return ending === strEnding;
 }
