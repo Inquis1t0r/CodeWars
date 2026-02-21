16a39ef47af76298a0475158c30dd975b1248124
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-02-21 19:55:09 +0100

    Update SumofMultiples.c

--- C/8_kyu/SumofMultiples.c
+++ C/8_kyu/SumofMultiples.c
@@ -1,8 +1,14 @@
 //  for invalid input, return -1
 
 int sum_mul(int n, int m) {
-
+    if(n <= 0 || m <= 0){
+      return -1;
+    }
     //  <----  hajime!
-
-    return -1;
+    int result = n;
+    
+    do{
+      result = result * 2;
+    } while(result < m);
+    return result;
 }
