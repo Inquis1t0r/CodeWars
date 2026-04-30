44ebfc9c2772a75e941323fde7334b370ea99d95
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-04-30 18:22:02 +0200

    Update Multiplythenumber.c

--- C/8_kyu/Multiplythenumber.c
+++ C/8_kyu/Multiplythenumber.c
@@ -1,5 +1,11 @@
+#include <math.h>
+#include <stdlib.h>
+
 int multiply(int number) {
+  if (number == 0) return 0;
+  int base = 5;
+  int digits = log10(abs(number)) + 1;
 
     //  <----  hajime!
-
+  return number * (int)round(pow(base, digits));
 }
