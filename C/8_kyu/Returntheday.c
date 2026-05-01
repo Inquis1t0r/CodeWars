2b01d63866998d46048a7d898a40291f50505a4f
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-01 09:31:56 +0200

    Update Returntheday.c

--- C/8_kyu/Returntheday.c
+++ C/8_kyu/Returntheday.c
@@ -1,5 +1,14 @@
 char* whatday(int num) {
-
+    if(num > 7 || num < 0){ return "Wrong, please enter a number between 1 and 7";}
     // <---- hajime!
-
+    char *days[7] = {
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    };
+  return days[num+1];
 }
