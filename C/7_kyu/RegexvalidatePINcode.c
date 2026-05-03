6a10de6a70928de12b1ae3208d088b9f20cb15a5
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-03 06:59:49 +0200

    Update RegexvalidatePINcode.c

--- C/7_kyu/RegexvalidatePINcode.c
+++ C/7_kyu/RegexvalidatePINcode.c
@@ -1,7 +1,23 @@
 #include <stdbool.h>
+#include <regex.h>  
+#include <stdio.h>
 
 bool validate_pin(const char *pin) {
 
     //  <----  hajime!
+    int len = strlen(pin);
+    if(len != 4 || len != 6){
+      return false;
+    }
+    regex_t regex;
+    const char *pattern = "^[0-9]+$"; 
+  
+    if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+      fprintf(stderr, "Failed to compile regex\n");
+      return false;
+    }
+    int match = regexec(&regex, pin, 0, NULL, 0);
+    regfree(&regex);
+    return match == 1 ? true : false;
 
 }
