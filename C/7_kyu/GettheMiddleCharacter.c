ab19cc938f213bb42ade7738d3bb44d78bcf3bd0
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-04 18:35:59 +0200

    Update GettheMiddleCharacter.c

--- C/7_kyu/GettheMiddleCharacter.c
+++ C/7_kyu/GettheMiddleCharacter.c
@@ -1,6 +1,17 @@
+#include <string.h>
 // Write to `outp`, nul-terminate it, and return it
 char *get_middle(char outp[3], const char *inp)
 {
-    *outp = '\0';
+    //*outp = '\0';
+    int len = strlen(inp);
+
+    if (len % 2 != 0) {
+        outp[0] = inp[len / 2];
+        outp[1] = '\0';
+    } else {
+        outp[0] = inp[(len / 2) - 1];
+        outp[1] = inp[len / 2];
+        outp[2] = '\0';
+    }
     return outp;
 }
