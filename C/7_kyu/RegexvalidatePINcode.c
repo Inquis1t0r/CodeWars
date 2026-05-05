7510ab7268b0ec9659503d2594ac59472730305d
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-05 06:11:29 +0200

    Fix PIN validation logic and improve regex handling

--- C/7_kyu/RegexvalidatePINcode.c
+++ C/7_kyu/RegexvalidatePINcode.c
@@ -6,18 +6,22 @@ bool validate_pin(const char *pin) {
 
     //  <----  hajime!
     int len = strlen(pin);
-    if(len != 4 || len != 6){
-      return false;
+    if (len != 4 && len != 6) {
+        return false;
     }
+
     regex_t regex;
-    const char *pattern = "^([0-9]{4}|[0-9]{6})$"; 
+    const char *pattern = "^([0-9]{4}|[0-9]{6})$";  
   
     if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
-      fprintf(stderr, "Failed to compile regex\n");
-      return false;
+        fprintf(stderr, "Failed to compile regex\n");
+        return false;
     }
+
     int match = regexec(&regex, pin, 0, NULL, 0);
     regfree(&regex);
-    return match == 1 ? true : false;
+
+
+    return (match == 0);
 
 }
