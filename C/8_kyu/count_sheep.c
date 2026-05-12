b48b641e0a8791a873c9e9bbb6c7dd0d070ae752
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-12 05:18:28 +0200

    Create count_sheep.c

---
+++ C/8_kyu/count_sheep.c
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/54edbc7200b811e956000556/train/c
+#include <stdbool.h>
+#include <stddef.h>
+
+size_t count_sheep(const bool sheep[/* count */], size_t count)
+{
+  return 0;
+}
