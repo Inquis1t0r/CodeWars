629b9f40a6483da94207520c54e0bf6e05bbf735
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-05-29 19:47:56 +0200

    Update ConverttoBinary.c

--- C/8_kyu/ConverttoBinary.c
+++ C/8_kyu/ConverttoBinary.c
@@ -1,4 +1,13 @@
 unsigned long long to_binary(unsigned short num)
 {
-    return 0ull;
+    unsigned long long d = 0ull;
+    unsigned long long placeValue = 1ull;
+
+    while (num > 0) {
+        d += (num % 2) * placeValue;
+        placeValue *= 10ull;
+        num /= 2;
+    }
+
+    return d;
 }
