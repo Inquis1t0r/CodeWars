a24ab5d6f6331d625b5b349c857c375205952381
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-08-15 07:47:49 +0200

    Update I love you, a little , a lot, passionately ... not at all.go

--- go/8_kyu/I love you, a little , a lot, passionately ... not at all.go
+++ go/8_kyu/I love you, a little , a lot, passionately ... not at all.go
@@ -1,5 +1,20 @@
 package kata
 
+import "fmt"
+
 func HowMuchILoveYou(i int) string {
-  return ""
+  phrases := [6]string{
+		"I love you",
+		"a little",
+		"a lot",
+		"passionately",
+		"madly",
+		"not at all",
+	}
+  
+  fmt.Println(i)
+  if(i>5){
+    i-=6
+  }
+  return phrases[i]
 }
