4eef72a0196f17ecac718f7346cdb5c1848f2d83
Inquis1t0r <pi0tr3k9292@gmail.com>
2026-08-16 14:18:27 +0200

    Update Beginner - Lost Without a Map.go

--- go/8_kyu/Beginner - Lost Without a Map.go
+++ go/8_kyu/Beginner - Lost Without a Map.go
@@ -1,13 +1,10 @@
 package kata
  	
-import "fmt"
 
 func Maps(x []int) []int {
-  //var result = [3]int
   for index,element := range x{
-    x[index]+= element*2
-    println(x[index])
+    x[index]+= element
   }
-  fmt.Println(x)
+  
   return x
 }
